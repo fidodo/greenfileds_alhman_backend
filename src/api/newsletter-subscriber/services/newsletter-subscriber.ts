d747c0e91b3a9c4e5ab409ebdcde60111b5bd5b6
ayokunle ogunfidodo <89479717+fidodo@users.noreply.github.com>
2026-05-12 11:01:57 +0300

    chore: remove unused newsletter-subscriber endpoint

--- src/api/newsletter-subscriber/services/newsletter-subscriber.ts
+++
@@ -1,7 +0,0 @@
-/**
- * newsletter-subscriber service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::newsletter-subscriber.newsletter-subscriber');
